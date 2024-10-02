3da411466c1d23426927f00f74fb24e2c253659b
Antoine Clarman <antoinereims28@gmail.com>
2024-10-03 01:32:26 +0200

    feat: add farm level and farm upgrade feature

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,80 @@
 use core::fmt;
 use std::io::{self, stdin, Write};
 
-struct Resources {
+struct Fortress {
     gold: i32,
     food: i32,
     wood: i32,
     energy: i32,
+    farm_level: i32,
 }
 
-impl Resources {
+impl Fortress {
     pub const fn new() -> Self {
         Self {
             gold: 0,
             food: 0,
             wood: 0,
             energy: 0,
+            farm_level: 1,
+        }
+    }
+
+    fn use_energy(&mut self, cost: i32) -> Result<(), ()> {
+        if self.energy >= cost {
+            self.energy -= cost;
+            Ok(())
+        } else {
+            println!("Not enough energy: {} < {cost}", self.energy);
+            Err(())
+        }
+    }
+
+    fn use_wood(&mut self, cost: i32) -> Result<(), ()> {
+        if self.wood >= cost {
+            self.wood -= cost;
+            Ok(())
+        } else {
+            println!("Not enough wood: {} < {cost}", self.wood);
+            Err(())
         }
     }
 
     fn earn_gold(&mut self) {
-        println!("The character ventures out to earn gold.");
-        self.gold += 1;
+        if self.use_energy(1).is_ok() {
+            println!("The character ventures out to earn gold.");
+            self.gold += 1;
+        }
     }
 
     fn gather_food(&mut self) {
-        println!("The character seeks to gather food.");
-        self.food += 1;
+        if self.use_energy(1).is_ok() {
+            println!("The character seeks to gather food.");
+            self.food += self.farm_level;
+        }
     }
 
     fn fell_trees(&mut self) {
-        println!("The character is going to chop wood.");
-        self.wood += 1;
+        if self.use_energy(1).is_ok() {
+            println!("The character is going to chop wood.");
+            self.wood += 1;
+        }
     }
 
     fn collect_energy(&mut self) {
         println!("The character sets out to collect energy.");
         self.energy += 1;
     }
+
+    fn improve_farm(&mut self) {
+        if self.use_wood(self.farm_level * 2).is_ok() {
+            println!("farm level increased!");
+            self.farm_level += 1;
+        }
+    }
 }
 
-impl fmt::Display for Resources {
+impl fmt::Display for Fortress {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
@@ -55,11 +90,11 @@ fn print_prompt() {
 }
 
 fn print_help() {
-    println!("0: help\n1: stats\n2: to earn gold\n3: to gather food\n4: to fell trees\n5: to collect energy\n9: exit");
+    println!("0: help\n1: stats\n2: to earn gold\n3: to gather food\n4: to fell trees\n5: to collect energy\n6: \n7: improve farm\n9: exit");
 }
 
 fn main() {
-    let mut resources = Resources::new();
+    let mut fortress = Fortress::new();
 
     print_help();
     print_prompt();
@@ -69,11 +104,12 @@ fn main() {
         match line.trim().parse() {
             Ok(n) => match n {
                 0 => print_help(),
-                1 => println!("{resources}"),
-                2 => resources.earn_gold(),
-                3 => resources.gather_food(),
-                4 => resources.fell_trees(),
-                5 => resources.collect_energy(),
+                1 => println!("{fortress}"),
+                2 => fortress.earn_gold(),
+                3 => fortress.gather_food(),
+                4 => fortress.fell_trees(),
+                5 => fortress.collect_energy(),
+                7 => fortress.improve_farm(),
                 9 => break,
                 _ => {}
             },
