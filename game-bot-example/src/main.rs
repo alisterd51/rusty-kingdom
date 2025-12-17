24d46872806bdc4f020d8699af6418996198ad8f
Antoine Clarman <aclarman@wiremind.io>
2025-12-17 14:10:49 +0100

    chore: simplify game proto
    BREAKING CHANGE: game.proto has been modified; a recompilation is required for the client to continue working

--- game-bot-example/src/main.rs
+++ game-bot-example/src/main.rs
@@ -1,7 +1,4 @@
-use crate::game::{
-    CollectFortressEnergyRequest, CollectFortressFoodRequest, CollectFortressGoldRequest,
-    CollectFortressWoodRequest, fortress_service_client::FortressServiceClient,
-};
+use crate::game::{CollectFortressRequest, fortress_service_client::FortressServiceClient};
 
 #[allow(clippy::pedantic)]
 #[allow(clippy::nursery)]
@@ -18,26 +15,22 @@ pub mod game {
 const SERVER_URL: &str = "https://rusty.anclarma.fr";
 const FORTRESS_ID: i32 = 42;
 
-#[allow(clippy::similar_names)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut fortress_client = FortressServiceClient::connect(SERVER_URL).await?;
-    let collect_gold_request = CollectFortressGoldRequest { id: FORTRESS_ID };
-    let collect_food_request = CollectFortressFoodRequest { id: FORTRESS_ID };
-    let collect_wood_request = CollectFortressWoodRequest { id: FORTRESS_ID };
-    let collect_energy_request = CollectFortressEnergyRequest { id: FORTRESS_ID };
+    let collect_request = CollectFortressRequest { id: FORTRESS_ID };
     for _ in 0..1000 {
         let _ = fortress_client
-            .collect_fortress_gold(collect_gold_request)
+            .collect_fortress_gold(collect_request)
             .await?;
         let _ = fortress_client
-            .collect_fortress_food(collect_food_request)
+            .collect_fortress_food(collect_request)
             .await?;
         let _ = fortress_client
-            .collect_fortress_wood(collect_wood_request)
+            .collect_fortress_wood(collect_request)
             .await?;
         let _ = fortress_client
-            .collect_fortress_energy(collect_energy_request)
+            .collect_fortress_energy(collect_request)
             .await?;
     }
     Ok(())
