11ab7a3d2a699e2843c3e4370127db0d2424fee7
Antoine Clarman <antoinereims28@gmail.com>
2026-01-31 00:34:43 +0100

    fix: minor code cleanups and simplifications

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,11 +1,11 @@
 use diesel::{Connection, PgConnection};
 use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../rusty/migrations/");
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../rusty/migrations/");
 
 fn establish_connection() -> Result<PgConnection, Box<dyn std::error::Error>> {
     let database_url = std::env::var("DATABASE_URL").map_err(|_| "DATABASE_URL must be set")?;
-    let conn = PgConnection::establish(&database_url).map_err(|e| format!("{e}"))?;
+    let conn = PgConnection::establish(&database_url).map_err(|e| e.to_string())?;
 
     Ok(conn)
 }
